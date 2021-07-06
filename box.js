fe41d118fb9edca88fd7785d9f4e4834ed911dec
Aarush1243 <86437515+Aarush1243@users.noreply.github.com>
2021-07-06 14:33:44 -0500

    Add files via upload

---
+++ box.js
@@ -0,0 +1,13 @@
+Class Box {
+  constructor(){
+    this.x = 200;
+    this.y = 200;
+    this.width = 30;
+    this.height = 30;
+  }
+  display(){
+    rect(this.x, this.y, this.width, this.height);
+  }
+}
+
+  
